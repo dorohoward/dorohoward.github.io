20b3588d618b3704a32c19ddc9b788ead6f51776
dorohoward <dorothywriting@outlook.com>
2025-10-06 10:45:48 +0800

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -4,9 +4,11 @@
 
 $(document).ready(function () {
   // detect OS/browser preference
-  const browserPref = window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light';
+  //const browserPref = window.matchMedia('(prefers-color-scheme: dark)').matches
+  //  ? 'dark'
+  //  : 'light';
+  // Remy replaced
+  const browserPref = 'dark'
 
   // Set the theme on page load or when explicitly called
   var setTheme = function (theme) {

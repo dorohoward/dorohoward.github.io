3a3233067284a6ab1e3a20bf9576539281ef3820
dorohoward <dorothywriting@outlook.com>
2025-10-06 10:51:24 +0800

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -4,19 +4,19 @@
 
 $(document).ready(function () {
   // detect OS/browser preference
-  //const browserPref = window.matchMedia('(prefers-color-scheme: dark)').matches
-  //  ? 'dark'
-  //  : 'light';
-  // Remy replaced
-  const browserPref = 'dark'
+  const browserPref = window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light';
 
   // Set the theme on page load or when explicitly called
   var setTheme = function (theme) {
-    const use_theme =
-      theme ||
-      localStorage.getItem("theme") ||
-      $("html").attr("data-theme") ||
-      browserPref;
+    // const use_theme =
+    //   theme ||
+    //   localStorage.getItem("theme") ||
+    //   $("html").attr("data-theme") ||
+    //   browserPref;
+    // Remy replaced <>
+    const use_theme = "dark"
 
     if (use_theme === "dark") {
       $("html").attr("data-theme", "dark");
